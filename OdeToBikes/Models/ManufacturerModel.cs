b0ac1961a6d84964ff83b7401a1e3be34b8c9c9d
StephenDever <harrisbicycle@gmail.com>
2017-01-19 23:14:37 -0800

    added some custom validation to be referenced for future use and expansion.

--- OdeToBikes/Models/ManufacturerModel.cs
+++ OdeToBikes/Models/ManufacturerModel.cs
@@ -6,16 +6,17 @@ using System.Web;
 
 namespace OdeToBikes.Models
 {
-    public class ManufacturerModel
+    public class ManufacturerModel : IValidatableObject
     {
         public int Id { get; set; }
 
         [Required]
         [StringLength(1024)]
         [Display(Name = "Model")]
+
         public string Name { get; set; }
 
-        [Range(1850, 2050)]
+        [Range(1850, 2050, ErrorMessage ="That's probably not right.")]
         [Required]
         public int Year { get; set; }
 
@@ -23,5 +24,14 @@ namespace OdeToBikes.Models
         [StringLength(1024)]
         public string Info { get; set; }
         public int ManufacturerId { get; set; }
+
+        // use this to validate what people can put into the Name field
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Name.ToLower().StartsWith("pos"))
+            {
+                yield return new ValidationResult("That's not nice!");
+            }
+        }
     }
 }
